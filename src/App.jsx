5cf05ed347bd6d704955801aa7c5ca2070a7c924
rehat <additislam@gmail.com>
2026-09-11 19:00:33 +0600

    Refactor App component to use Home page and add new components for Stack and Technology sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import Footer from "./components/Footer/Footer"
-import Hero from "./components/Hero/Hero"
-import Navbar from "./components/Navbar/Navbar"
+import Home from "./pages/Home"
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Hero/>
-      <Footer/>
+      <Home/>   
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer/Footer"
+import Hero from "./components/Hero/Hero"
+import Navbar from "./components/Navbar/Navbar"
+
+function Home() {
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home
